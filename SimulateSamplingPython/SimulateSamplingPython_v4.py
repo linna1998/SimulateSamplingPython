0bce2e0d1fbace08d8b42c34f44c9a56f8f10513
linna1998 <1500012782@pku.edu.cn>
2018-08-07 14:10:50 -0700

    Add the function to solve non-linear situations. When it finds a non-linear constraint, it will exit happily according to the code.

--- SimulateSamplingPython/SimulateSamplingPython_v4.py
+++ SimulateSamplingPython/SimulateSamplingPython_v4.py
@@ -28,8 +28,8 @@ def getValue(constraintsId, variables):
 def simulateFunction(variables):
     returnValueList = []
     for i in range(0, constraintsNum):
-        returnValue = getValue(i, variables);
-        returnValueList.append(returnValue);
+        returnValue = getValue(i, variables)
+        returnValueList.append(returnValue)
         if (returnValue.isValid == False):
            break
     return returnValueList
@@ -41,13 +41,14 @@ def solve_ilp(objective, constraints):
     prob += objective
     for cons in constraints:
         prob += cons
-    print("prob", prob);    
-    status = prob.solve(pulp.GLPK(options=['--mipgap', '0.000001', '--cgr']));
+    print("prob", prob)    
+    status = prob.solve(pulp.GLPK(options=['--mipgap', '0.000001', '--cgr']))
     # status = prob.solve(pulp.COIN_CMD(fracGap = 0));
-    # status = prob.solve(pulp.PULP_CBC_CMD());  # default
+    # status = prob.solve(pulp.PULP_CBC_CMD()); # default
     if status != 1:
         # print('status', status)
-        # LpStatus = {-3: 'Undefined', -2: 'Unbounded', -1: 'Infeasible', 0: 'No...
+        # LpStatus = {-3: 'Undefined', -2: 'Unbounded', -1: 'Infeasible', 0:
+        # 'No...
         return None
     else:
         return [v.varValue.real for v in prob.variables()]
@@ -61,53 +62,26 @@ def getVariables(constraintsId, parameter_isEqual,
     if constraintsId == 0:
          for j in range(0, variablesNum):
             variables.append(np.random.randint(variables_lower[j], high = variables_upper[j]))
-         return variables, block, position_list
+         return variables
     else:
         # Randomly get a variable
         # which suits for 0 ~ constraintsId - 1 constraints
         V_NUM = variablesNum + 1
         
         # Variables
-        pulp_variables = [];
-        for i in range(1, variablesNum + 1):
-            ## the first possible solution
-            #temp_lowBound = np.random.randint(variables_lower[i - 1], high = variables_upper[i - 1]);
-            #temp_upBound = np.random.randint(variables_lower[i - 1], high = variables_upper[i - 1]);                     
-            #if (temp_lowBound >= temp_upBound):
-            #    temp = temp_lowBound;
-            #    temp_lowBound = temp_upBound;
-            #    temp_upBound = temp;                
-            ## the second possible solution
-            #temp_lowBound = round(variables_lower[i-1] * np.random.rand());
-            #temp_upBound = round(variables_upper[i-1] * np.random.rand()); 
-            
-            # the third possible solution
-            # using block & position_list to divide the space into several parts
-            partialLength = (variables_upper[i-1] - variables_lower[i-1]) / block;
-            temp_lowBound = round(variables_lower[i-1] + partialLength * position_list[i-1]);
-            temp_upBound = round(temp_lowBound + partialLength);            
+        pulp_variables = []
+        for i in range(1, variablesNum + 1):                                   
+            # using block & position_list to divide the space into several
+            # parts
+            partialLength = (variables_upper[i - 1] - variables_lower[i - 1]) / block
+            temp_lowBound = max(round(variables_lower[i - 1] + partialLength * position_list[i - 1]), variables_lower[i - 1])
+            temp_upBound = min(round(temp_lowBound + partialLength), variables_upper[i - 1])            
 
             pulp_variables.append(pulp.LpVariable('X%d' % i, cat = pulp.LpInteger, 
-                                                  lowBound = temp_lowBound, upBound = temp_upBound));
-            
-            
-        
+                                                  lowBound = temp_lowBound, upBound = temp_upBound))
+
         pulp_variables.append(pulp.LpVariable('t', cat = pulp.LpInteger))
         # print("pulp_variables", pulp_variables)
-
-        # calculate the carry
-        position_list[variablesNum-1] += 1;
-        final_input = 0;
-        for i in range(variablesNum-1, -1, -1):
-            if (position_list[i] >= block):
-                position_list[i] -= block;
-                if (i > 0):
-                    position_list[i-1] += 1;
-                else:
-                    final_input = 1;
-        if (final_input == 1):
-            position_list = [0] * variablesNum;
-            block = block * 2;
 
         # Objective function
         F = [0] * variablesNum
@@ -123,17 +97,11 @@ def getVariables(constraintsId, parameter_isEqual,
             temp_b = temp_a[variablesNum]
             # add fi - t <= 0
             temp_a[variablesNum] = -1
-            constraints.append(sum([temp_a[i] * pulp_variables[i] for i in range(0, V_NUM)]) <= -temp_b)
-            # add fi <= 0
-            temp_a[variablesNum] = -1
-            constraints.append(sum([temp_a[i] * pulp_variables[i] for i in range(0, V_NUM)]) <= -temp_b)
+            constraints.append(sum([temp_a[i] * pulp_variables[i] for i in range(0, V_NUM)]) <= -temp_b)            
             if (parameter_isEqual[k]):   
                 # add fi + t >= 0 [for the equation constraints
                 temp_a[variablesNum] = 1
-                constraints.append(sum([temp_a[i] * pulp_variables[i] for i in range(0, V_NUM)]) >= -temp_b) 
-                ##  add fi == 0
-                temp_a[variablesNum] = 0;
-                constraints.append(sum([temp_a[i] * pulp_variables[i] for i in range(0, V_NUM)]) == -temp_b) 
+                constraints.append(sum([temp_a[i] * pulp_variables[i] for i in range(0, V_NUM)]) >= -temp_b)  
         temp_v = [0] * variablesNum
         for k in range(0, variablesNum):
             temp_v[k] = 1
@@ -144,28 +112,14 @@ def getVariables(constraintsId, parameter_isEqual,
 
         variables = solve_ilp(objective, constraints)
         # print("variables", variables)
-
-        ## TODO 怎么设置停机时间   
-        #while (True):     
-        #    if (variables == None):
-        #        # no possible solution for the ilp problem.
-        #        print("No possible solution! Test another random constraints QAQ")
-        #        variables = getVariables(constraintsId, parameter_isEqual, 
-        #             variablesNum, variables_lower, variables_upper, parameter_result);
-        #        return variables
-        #    # delete the last "t" variable
-        #    variables.pop()
-        #    # ganrantee the variables fit the constraints
-        #    if (len(returnValueList) == constraintsId and constraintsId == constraintsNum and returnValueList[constraintsId - 1].isValid):
-        #       return variables;
-
+        
         print("variables", variables)
         if (variables == None):
             print("No possible solution! QAQ")
-            return variables, block, position_list
+            return variables
         # delete the last "t" variable
         variables.pop()
-        return variables, block, position_list          
+        return variables         
     
 def matlabSolver(A, B):
     eng = matlab.engine.start_matlab()
@@ -185,44 +139,71 @@ def solveConstraint(constraintsId, parameter_isEqual, constraintsNum,
     # in linear equations
     A = []
     B = []
-    equalNum = 0;
+    equalNum = 0
     for i in range(0, constraintsId):
         if (parameter_isEqual[i]):
             equalNum += 1;
 
     # used in select a possible variables
-    block = 2;
-    position_list = [0] * variablesNum;
+    block = 1
+    position_list = [0] * variablesNum
+    isLinear = 0;
 
     while (len(A) < variablesNum + 1):        
-        variables, block, position_list = getVariables(constraintsId, parameter_isEqual, 
+        variables = getVariables(constraintsId, parameter_isEqual, 
                 variablesNum, variables_lower, variables_upper, parameter_result,
-                block, position_list);   
+                block, position_list);
         sampling_count += 1;
+        
+        # decide whether it is non-linear
+        # when position_list = [0, 0, ..., 0]
+        if (position_list == [0] * variablesNum and block > 1):
+            if (isLinear == 0):
+                print("Non-linear constraints! No possible solutions! QAQ")
+                return parameter_result, sampling_count, None
+            isLinear = 0;
+        
+        # calculate the carry, update position_list and block
+        position_list[variablesNum - 1] += 1;
+        final_input = 0;
+        for i in range(variablesNum - 1, -1, -1):
+            if (position_list[i] >= block):
+                position_list[i] -= block
+                if (i > 0):
+                    position_list[i - 1] += 1
+                else:
+                    final_input = 1;
+        if (final_input == 1):            
+            position_list = [0] * variablesNum
+            block = block * 2;
+            
+      
         if variables == None:
-            print("No possible solution in these constraints QAQ");
-            continue;        
-
-        returnValueList = simulateFunction(variables);
+            print("No possible solution in these constraints QAQ")
+            continue        
+                
+        returnValueList = simulateFunction(variables)
 
         if (len(returnValueList) == constraintsId and constraintsId == constraintsNum and returnValueList[constraintsId - 1].isValid):
         # give the final input for the original constraints
-            print("Final input for the fuzzer:", variables);
+            print("Final input for the fuzzer:", variables)
             return parameter_result, sampling_count, variables
 
         if (len(returnValueList) <= constraintsId):
             print("The solution variable is not suitable for the problem. QAQ")
-            print(variables);
+            print(variables)
             continue
         
-        variables.append(1);
-        A.append(variables);
+        variables.append(1)
+        A.append(variables)
         # guarantee the linear independence
-        rank = np.linalg.matrix_rank(A);
+        rank = np.linalg.matrix_rank(A)
         if (rank < len(A) and rank < variablesNum + 1 - equalNum):
-            A.pop();
-            continue;
-
+            A.pop()
+            continue
+        
+        # add a line into matrix A
+        isLinear = 1;
         B.append(returnValueList[constraintsId].intValue)
 
     print("A = ", A)
@@ -241,37 +222,6 @@ MIN = -2147483648
 MAX = 1000
 MIN = -1000
 
-# g1(x1; x2) := x1 - x2 == 0
-# if (x1 - x2 == 0) {
-#parameter_a = [[1, -1]]
-#parameter_b = [0]
-#parameter_isEqual = [True]
-#constraintsNum = 1
-
-#variablesNum = 2
-#variables_lower = [MIN, MIN]
-#variables_upper = [MAX, MAX]
-
-##
-## f1(x1; x2) := x1 + 1 <= 0
-## g1(x1; x2) := x1 - x2 == 0
-## if (x1 - x2 == 0) {
-##      if (x1 + 1 <= 0) {
-#parameter_a = [[1, -1], [1, 0]]
-#parameter_b = [0, 1]
-#parameter_isEqual = [True, False]
-#constraintsNum = 2
-
-#variablesNum = 2
-#variables_lower = [MIN, MIN]
-#variables_upper = [MAX, MAX]
-
-#parameter_a = [[1, -1, 0], [1, 0, 0]]
-#parameter_b = [2, 3]
-#parameter_isEqual = [True, False]
-#constraintsNum = 2
-##
-
 parameter_a = [[1, -1, 0], [1, 0, 0], [0, 1, 1]]
 parameter_b = [2, 3, 4]
 parameter_isEqual = [True, False, True]
@@ -281,17 +231,20 @@ variablesNum = 3
 variables_lower = [MIN, MIN, MIN]
 variables_upper = [MAX, MAX, MAX]
 
-total_count = 0;
-exeTimes = 1;
+total_count = 0
+exeTimes = 1
 for T in range(0, exeTimes):
     parameter_result = []  # the result of simulation
     sampling_count = 0 # the number of sampling
     for i in range(0, constraintsNum + 1):
         parameter_result, sampling_count, variables = solveConstraint(i, parameter_isEqual, 
                                                                       constraintsNum, variablesNum, variables_lower, 
-                                                                      variables_upper, parameter_result, sampling_count);
-    print("sampling counts: ", sampling_count);
-    total_count = total_count + sampling_count;
+                                                                      variables_upper, parameter_result, sampling_count)
+        if (variables == None):
+            print("Non-linear constraints! TAT");
+            break;
+    print("sampling counts: ", sampling_count)
+    total_count = total_count + sampling_count
 
 print("parameter_result", parameter_result)
 print(exeTimes, " total counts: ", total_count);
